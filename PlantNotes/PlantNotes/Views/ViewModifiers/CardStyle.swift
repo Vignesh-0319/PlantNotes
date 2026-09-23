e6cc3984596b7031dd564c07595d41bbb0cf2f01
Vignesh <vignesh0319.becse24@chitkara.edu.in>
2026-09-23 10:11:36 +0530

    New changes

--- PlantNotes/PlantNotes/Views/ViewModifiers/CardStyle.swift
+++ PlantNotes/PlantNotes/Views/ViewModifiers/CardStyle.swift
@@ -5,3 +5,20 @@
 //  Created by SUPER CHARGE on 23/09/26.
 //
 
+import SwiftUI
+
+struct CardStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(Color.green.opacity(0.12))
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+    }
+}
+
+extension View {
+    func cardStyle() -> some View {
+        modifier(CardStyle())
+    }
+}
