6fea278bcae46e749580557bfdf4d8dc85e2e546
Vignesh <vignesh0319.becse24@chitkara.edu.in>
2026-09-23 10:14:03 +0530

    New changes

--- PlantNotes/PlantNotes/Views/ReusableViews/DetailRow.swift
+++ PlantNotes/PlantNotes/Views/ReusableViews/DetailRow.swift
@@ -5,3 +5,18 @@
 //  Created by SUPER CHARGE on 23/09/26.
 //
 
+import SwiftUI
+
+struct DetailRow: View {
+    let label: String
+    let value: String
+
+    var body: some View {
+        HStack {
+            Text(label)
+            Spacer()
+            Text(value)
+                .foregroundStyle(.secondary)
+        }
+    }
+}
