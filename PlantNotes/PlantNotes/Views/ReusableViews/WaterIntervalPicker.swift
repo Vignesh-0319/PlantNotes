0c63a0a75fb5f822df2178dce975b2ca66322346
Vignesh <vignesh0319.becse24@chitkara.edu.in>
2026-09-23 10:14:45 +0530

    New changes

--- PlantNotes/PlantNotes/Views/ReusableViews/WaterIntervalPicker.swift
+++ PlantNotes/PlantNotes/Views/ReusableViews/WaterIntervalPicker.swift
@@ -5,3 +5,21 @@
 //  Created by SUPER CHARGE on 23/09/26.
 //
 
+import SwiftUI
+
+struct WaterIntervalPicker: View {
+    @Binding var days: Int
+
+    var body: some View {
+        HStack {
+            Text("Water every")
+            Spacer()
+            Picker("Water every", selection: $days) {
+                ForEach(2...14, id: \.self) { number in
+                    Text("\(number) days").tag(number)
+                }
+            }
+            .pickerStyle(.menu)
+        }
+    }
+}
