3633144bcbef6d3f7800ff651d024c8507edb722
Vignesh <vignesh0319.becse24@chitkara.edu.in>
2026-09-23 10:14:15 +0530

    New changes

--- PlantNotes/PlantNotes/Views/ReusableViews/WaterButton.swift
+++ PlantNotes/PlantNotes/Views/ReusableViews/WaterButton.swift
@@ -5,3 +5,21 @@
 //  Created by SUPER CHARGE on 23/09/26.
 //
 
+import SwiftUI
+
+struct WaterButton: View {
+    let plant: Plant
+    @Environment(GardenStore.self) private var store
+
+    var body: some View {
+        Button {
+            store.water(plant)
+        } label: {
+            HStack {
+                Image(systemName: "drop.fill")
+                Text(plant.wateredToday ? "Watered today" : "Water now")
+            }
+        }
+        .disabled(plant.wateredToday)
+    }
+}
