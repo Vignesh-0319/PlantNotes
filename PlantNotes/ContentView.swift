e59af13556542a5c91f79436963dda963b946e39
Vignesh <vignesh0319.becse24@chitkara.edu.in>
2026-09-23 09:39:01 +0530

    Initial Commit

---
+++ PlantNotes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PlantNotes
+//
+//  Created by SUPER CHARGE on 23/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PlantNotes/PlantNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantNotesApp.swift
+//  PlantNotes
+//
+//  Created by SUPER CHARGE on 23/09/26.
+//
+
+import SwiftUI
+
+@main
+struct PlantNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
