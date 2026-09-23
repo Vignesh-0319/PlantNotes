2cae2db24e3fa0f1a4d966f8c5c9ab47e5eb2c54
Vignesh <vignesh0319.becse24@chitkara.edu.in>
2026-09-23 10:14:31 +0530

    New changes

--- PlantNotes/PlantNotes/Views/ReusableViews/CareActionsSection.swift
+++ PlantNotes/PlantNotes/Views/ReusableViews/CareActionsSection.swift
@@ -5,3 +5,18 @@
 //  Created by SUPER CHARGE on 23/09/26.
 //
 
+import SwiftUI
+
+struct CareActionsSection: View {
+    let plant: Plant
+
+    var body: some View {
+        Section {
+            WaterButton(plant: plant)
+        } header: {
+            Text("Care Actions")
+        } footer: {
+            Text("Watering resets the countdown to the next watering.")
+        }
+    }
+}
